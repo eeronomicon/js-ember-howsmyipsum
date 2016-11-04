5713b02d9cfca57893b488331449109c0133f507
HK Kahng <hk@router-rooter.com>
2016-11-04 10:23:32 -0700

    add computed property of average ratings to Ipsum model

--- app/models/ipsum.js
+++ app/models/ipsum.js
@@ -5,5 +5,16 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   content: DS.attr('string'),
   dateCreated: DS.attr('date'),
-  reviews: DS.hasMany('review')
+  reviews: DS.hasMany('review'),
+  averageRating: Ember.computed(function() {
+    var returnArray = [];
+    var thisModel = this;
+    var returnValue = this.get('reviews').then(function(reviews) {
+      reviews.forEach(function(review) {
+        returnArray.push(review.get('rating'));
+      });
+      var average = (returnArray.length > 0 ? returnArray.reduce(function(a, b) { return a + b; }, 0) / returnArray.length : 0);
+      thisModel.set('averageRating', average.toFixed(2));
+    });
+  })
 });
